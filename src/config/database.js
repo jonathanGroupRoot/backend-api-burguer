d696d884136edd3ba08fb423f519a1af5d90b07f
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-06-22 17:57:48 -0300

    Adicionando Validates

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-    host: 'mysql669.umbler.com', 
-    database: 'burguer_grill',
-    username: 'jonathan',
-    password: '78951fla',
-    dialect: 'mysql',
+    host: 'ec2-34-200-15-192.compute-1.amazonaws.com', 
+    database: 'dqhgtplinludd',
+    username: 'aejvzfnffbtmzw',
+    password: '7bf495f98b388f190b0495308e463e3aa35c2b6619abb1bc3d15feb47bf89c6c',
+    dialect: 'pgsql',
     define: {
         timestamps: true,
         underscored: true,
